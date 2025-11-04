6557ea73b1aac1fddf3463a3e747dd44ed7dd35e
zeus0325 <zeus0325@gmail.com>
2025-11-03 23:33:57 -0800

    Update heap.h

--- heap.h
+++ heap.h
@@ -5,31 +5,31 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-#include <iostream>
-using namespace std;
+extern int weightArr[];
 
 struct MinHeap {
-    int data[64];
+    int data[512];
     int size;
-
-    MinHeap() { size = 0; }
-
-    void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+    MinHeap(){ size=0; }
+    int top(){ return data[0]; }
+    void push(int x){ data[size]=x; upheap(size++); }
+    void pop(){ data[0]=data[--size]; downheap(0); }
+    void upheap(int i){
+        while(i>0){
+            int p=(i-1)/2;
+            if(weightArr[data[i]]<weightArr[data[p]]){
+                int t=data[i]; data[i]=data[p]; data[p]=t; i=p;
+            } else break;
+        }
     }
-
-    int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
-    }
-
-    void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
-    }
-
-    void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+    void downheap(int i){
+        while(true){
+            int l=2*i+1,r=2*i+2,s=i;
+            if(l<size && weightArr[data[l]]<weightArr[data[s]]) s=l;
+            if(r<size && weightArr[data[r]]<weightArr[data[s]]) s=r;
+            if(s!=i){ int t=data[i]; data[i]=data[s]; data[s]=t; i=s; }
+            else break;
+        }
     }
 };
 
